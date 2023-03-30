d5f07138b7726caf480b2da2b5daa9e0f6a82792
Nikolai Potiashin <nikolai_potiashin@epam.com>
2023-03-30 22:49:48 +0300

    add tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,49 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static final int MAX_LENGTH = 80;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Type some text:");
+        var reader = new BufferedReader( new InputStreamReader(System.in));
+        var input = reader.readLine();
+        System.out.println("Output: " + splitString(input));
     }
+
+    public static String splitString(String input) {
+        if (input == null || input.isBlank()) {
+            return "";
+        }
+        var output = new StringBuilder();
+        var line = new StringBuilder();
+        String[] words = input.split(" ");
+
+        for (var word : words) {
+            if (word.isEmpty() || word.length() > MAX_LENGTH) {
+                continue;
+            }
+            if (line.length() + word.length() <= MAX_LENGTH) {
+                line.append(word).append(" ");
+            } else {
+                writeLineToOutput(output, line);
+                line = new StringBuilder();
+                line.append(word).append(" ");
+            }
+        }
+        writeLineToOutput(output, line);
+        return output.toString();
+    }
+
+    private static void writeLineToOutput(StringBuilder output, StringBuilder line) {
+        if (!line.isEmpty()) {
+            line.replace(line.length() - 1, line.length(), "\n");
+        }
+        output.append(line);
+    }
+
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,67 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.example;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.example.Main.MAX_LENGTH;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class MainTest {
-  
+
+    private static final int WORDS_COUNT = 100;
+    private static final String STRING80 =
+            "abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij"; //80 symbols
+    private final String regex = "^(?:[a-zA-Z](?:[a-zA-Z ]{0,78}[a-zA-Z])?\\n)+$";
+
+    private static final Random random = new Random();
+
+    @Test
+    @RepeatedTest(500)
+    public void shouldSplitRandomString() {
+        var input = generateRandomString();
+        var output = Main.splitString(input);
+        System.out.println("INPUT: " + input);
+        System.out.println("\nOUTPUT: = " + output);
+
+        assertTrue(output.matches(regex));
+    }
+
+    @Test
+    public void shouldSplitStringWithMaxSymbols() {
+        assertTrue(Main.splitString(STRING80).matches(regex));
+    }
+    @Test
+    public void shouldReturnEmptyStringForIncorrectInput() {
+        assertEquals("", Main.splitString(""));
+        assertEquals("", Main.splitString("  "));
+        assertEquals("", Main.splitString(null));
+        assertEquals("", Main.splitString(STRING80 + "a"));
+    }
+
+    private String generateRandomString() {
+        String[] words = new String[WORDS_COUNT];
+        for (int i = 0; i < WORDS_COUNT; i++) {
+            int wordLength = random.nextInt(MAX_LENGTH) + 1;
+            StringBuilder word = new StringBuilder();
+            for (int j = 0; j < wordLength; j++) {
+                char c = (char) (random.nextInt(26) + 'a');
+                word.append(c);
+            }
+            words[i] = word.toString();
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < WORDS_COUNT; i++) {
+            result.append(words[i]);
+            int numSpaces = random.nextInt(5) + 1;
+            for (int j = 0; j < numSpaces; j++) {
+                result.append(' ');
+            }
+        }
+        return result.toString();
+    }
 }
